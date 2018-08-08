a58cafc1bf5038f8916f5b0d5ff7b81d59fbd1d1
Lazy-dog <y371430933@163.com>
2018-08-08 17:23:30 +0800

    一个简单的打包

---
+++ Webpack/webpack.config.js
@@ -0,0 +1,10 @@
+const path=require('path')
+
+module.exports={
+	entry:"./src/app.js", //默认通过查找webpack.config.js的文件进行打包 'webpack --config 自己取得配置文件名'
+	output:{
+		path:path.resolve(__dirname,'dist'), //__dirname表示当前文件目录的绝对路径,'dist'表示打包后的位置
+		filename:'main.js' //打包后的文件名
+	}, 
+	mode:"development"  //打包模式：development还是production(压缩的)(webpack4.0新增的)
+}
