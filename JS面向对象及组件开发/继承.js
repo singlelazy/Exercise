e135fe0621af6617a7b5453d5687db246ac4476f
Lazy-dog <y371430933@163.com>
2018-08-18 20:58:35 +0800

    继承的基本知识以及demo继承拖拽

--- JS面向对象及组件开发/继承.js
+++ JS面向对象及组件开发/继承.js
@@ -1,7 +1,7 @@
 //属性的继承，调用父亲的构造函数，用call更改指向
 //方法的继承：for in 拷贝继承
 function CreatePerson(name,sex){   //父类
-	this.name=name;
+	this.name="1";
 	this.sex=sex;
 }
 CreatePerson.prototype.showName=function(){
@@ -15,6 +15,11 @@ function CreateStar(name,sex,job){
 	CreatePerson.call(this,name,sex)
 	this.job=job
 }
-CreateStar.prototype=CreatePerson.prototype
+extend(CreateStar.prototype,CreatePerson.prototype)
 var p2=new CreateStar("雷军","男","耍猴");
 
+function extend(obj1,obj2){
+	for(var attr in obj2){
+		obj1[attr]=obj2[attr];
+	}
+}	
