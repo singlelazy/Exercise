86a0c92185b4230072b57fb450dafe2917d62a7b
singlelazy <y371430933@163.com>
2018-08-25 19:08:15 +0800

    新增字体打包

--- Webpack/src/app.js
+++ Webpack/src/app.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './common/style/main.css'
 import book from './common/img/book.jpg'
+import 'font-awesome/css/font-awesome.css'
 
 ReactDOM.render(
-	<div className="ret">React
+	<div className="fa fa-rocket">React
 		<img src={book} />
 	</div>,
 	document.getElementById('root')
--- Webpack/webpack.config.js
+++ Webpack/webpack.config.js
@@ -27,13 +27,26 @@ module.exports={
 					}
 				]
 			},
+			//处理CSS文件中出现的URL，会自动帮你引入里面重要的模块
 			{
 				test:/\.css$/,
 				use:['style-loader','css-loader']
 			},
+			//file-loader:
+			//	1.把你的资源移动到输出目录
+			//	2.返回最终引入资源的URL
 			{
 				test:/\.(jpg|gif|png|jpeg)$/,
-				use:['url-loader']
+				use:[{
+					loader:'url-loader',
+					options:{
+						limit:10000 //Byte为单位
+					}
+				}]
+			},
+			{
+				test:/\.(ttf|eot|woff|woff2|svg)$/,
+				use:['file-loader']
 			}
 		]
 	},
